8e2b2957bee909ac4c70db00c92a27be81c41bf7
Robin Ridderholt <robin.ridderholt@live.com>
2013-12-01 22:27:13 +0100

    Created attribute and metadata file for the User

---
+++ CentralizedValidation.Web/Code/Attribute/MetadataLocationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CentralizedValidation.Web.Code.Attribute
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MetadataLocationAttribute : System.Attribute
+    {
+        public Type Type { get; set; }
+
+        public MetadataLocationAttribute(Type type)
+        {
+            Type = type;
+        }
+    }
+}
---
+++ CentralizedValidation.Web/Code/Metadata/UserMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CentralizedValidation.Web.Code.Metadata
+{
+    public class UserMetadata
+    {
+        [Display(Name = "Firstname")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Firstname is required")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Lastname")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Lastname is required")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+    }
+}
