3085861ff048b6b48333584c3221ccfaabf0a77e
Robin Ridderholt <robin.ridderholt@live.com>
2013-12-01 21:56:57 +0100

    Created Custom metadata provider class

---
+++ CentralizedValidation.Web/Code/CustomMetadataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.ModelBinding;
+
+namespace CentralizedValidation.Web.Code
+{
+    public class CustomMetadataProvider : DataAnnotationsModelMetadata
+    {
+        public CustomMetadataProvider(DataAnnotationsModelMetadataProvider provider, Type containerType, Func<object> modelAccessor, Type modelType, string propertyName, DisplayColumnAttribute displayColumnAttribute)
+            : base(provider, containerType, modelAccessor, modelType, propertyName, displayColumnAttribute)
+        {
+
+        }
+   }
+}
