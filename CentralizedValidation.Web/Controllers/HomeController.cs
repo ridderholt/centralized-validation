170aff1c43669d9f7b6bf451c90fd2c1c39e32dd
Robin Ridderholt <robin.ridderholt@live.com>
2013-12-01 19:51:16 +0100

    Created form for adding a user

--- CentralizedValidation.Web/Controllers/HomeController.cs
+++ CentralizedValidation.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CentralizedValidation.Web.Models;
 
 namespace CentralizedValidation.Web.Controllers
 {
@@ -10,7 +11,7 @@ namespace CentralizedValidation.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new UserViewModel());
         }
 
         public ActionResult About()
