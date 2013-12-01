2cff35f305fa3b43a0fcd258f8d9fbfb9bdd1c5f
Robin Ridderholt <robin.ridderholt@live.com>
2013-12-01 19:51:28 +0100

    Adds UserViewModel

---
+++ CentralizedValidation.Web/Models/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CentralizedValidation.Web.Models
+{
+    public class UserViewModel
+    {
+        [Display(Name = "Firstname")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Lastname")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+    }
+}
