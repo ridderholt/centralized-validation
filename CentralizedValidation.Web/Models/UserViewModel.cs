fa0a174b6e7f146f20e87f2615b553f1f029de57
Robin Ridderholt <robin.ridderholt@live.com>
2013-12-01 19:55:27 +0100

    Adds some generall validation rules

--- CentralizedValidation.Web/Models/UserViewModel.cs
+++ CentralizedValidation.Web/Models/UserViewModel.cs
@@ -5,9 +5,11 @@ namespace CentralizedValidation.Web.Models
     public class UserViewModel
     {
         [Display(Name = "Firstname")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Firstname is required")]
         public string Firstname { get; set; }
 
         [Display(Name = "Lastname")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Lastname is required")]
         public string Lastname { get; set; }
 
         [Display(Name = "E-mail")]
