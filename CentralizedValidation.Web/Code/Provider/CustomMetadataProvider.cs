9292dd8c74877cd8b04ba7e6983c20089ed44175
Robin Ridderholt <robin.ridderholt@live.com>
2013-12-02 18:28:29 +0100

    Added Validator and Provider

---
+++ CentralizedValidation.Web/Code/Provider/CustomMetadataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using CentralizedValidation.Web.Code.Attribute;
+
+namespace CentralizedValidation.Web.Code.Provider
+{
+    public class CustomMetadataProvider : DataAnnotationsModelMetadataProvider
+    {
+        protected override ModelMetadata CreateMetadata(IEnumerable<System.Attribute> attributes, Type containerType, Func<object> modelAccessor, Type modelType, string propertyName)
+        {
+            if (attributes.OfType<MetadataLocationAttribute>().Any())
+            {
+                var metadataType = attributes.OfType<MetadataLocationAttribute>().First().Type;
+
+                if (attributes != null)
+                    return base.CreateMetadata(attributes, metadataType, modelAccessor, modelType, propertyName);
+            }
+
+            return base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
+        }
+    }
+}
---
+++ CentralizedValidation.Web/Code/Validator/ValidatorBase.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Resources;
+using CentralizedValidation.Web.Code.Attribute;
+
+namespace CentralizedValidation.Web.Code.Validator
+{
+    public abstract class ValidatorBase<TEntity> where TEntity : class
+    {
+        public IEnumerable<ValidationResult> ValidationResult { get; protected set; }
+
+        public virtual bool IsValid(TEntity entity)
+        {
+            var results = new List<ValidationResult>();
+
+            foreach (var propertyInfo in entity.GetType().GetProperties())
+            {
+                var metaDataAttributes = propertyInfo.GetCustomAttributes(typeof(MetadataLocationAttribute), true);
+                if (metaDataAttributes.Any())
+                {
+                    var metaAttribute = metaDataAttributes[0] as MetadataLocationAttribute;
+                    var metaDataInfo = metaAttribute.Type.GetProperty(propertyInfo.Name);
+                    var validationAttributes = metaDataInfo.GetCustomAttributes(typeof(ValidationAttribute), true);
+
+                    foreach (var attr in validationAttributes)
+                    {
+                        var validationAttribute = attr as ValidationAttribute;
+                        var isValid = validationAttribute.IsValid(propertyInfo.GetValue(entity, null));
+
+                        if (!isValid)
+                        {
+                            var members = new List<string> { propertyInfo.Name };
+
+                            if (string.IsNullOrEmpty(validationAttribute.ErrorMessageResourceName))
+                            {
+                                results.Add(new ValidationResult(validationAttribute.ErrorMessage, members));
+                            }
+                            else
+                            {
+                                var manager = new ResourceManager(validationAttribute.ErrorMessageResourceType);
+                                results.Add(new ValidationResult(manager.GetString(validationAttribute.ErrorMessageResourceName), members));
+                            }
+                        }
+                    }
+                }
+            }
+
+            ValidationResult = results;
+            return !results.Any();
+        }
+    }
+}
