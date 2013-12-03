5c906d9770d5f18cbfb6149858ed542b260cc52d
Robin Ridderholt <robin.ridderholt@live.com>
2013-12-03 20:54:38 +0100

    Removed unused actions and changes application name

--- CentralizedValidation.Web/Controllers/HomeController.cs
+++ CentralizedValidation.Web/Controllers/HomeController.cs
@@ -29,19 +29,5 @@ namespace CentralizedValidation.Web.Controllers
 
             return View(model);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+   }
 }
